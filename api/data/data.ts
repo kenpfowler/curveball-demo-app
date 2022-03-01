f6687a24ee28b60a0d4858729f27f14fab944d72
Ken Fowler <development@kennyf.ca>
2022-02-28 23:00:16 -0500

    added mock data

--- api/data/data.ts
+++ api/data/data.ts
@@ -1,4 +1,4 @@
-import {User} from "../types";
+import {User, Campagin, Category} from "../types";
  
 export const data: User[] = [
    {
@@ -37,3 +37,42 @@ export const data: User[] = [
     campaigns: []
   },
 ];
+
+export const campaginData: Campagin[] = [
+  {
+    title: "Monero Fund",
+    category: [{type: "Reasearch & Development"}],
+    description: "Money to fund the monero project",
+    bannerImage: "mockbannerimages.com",
+    goalAmount: 50,
+    updates: "string",
+    shareLink: "monero-fund",
+    owner: data[0]
+
+  },
+  {
+    title: "Bitcoin Fund",
+    category: [{type: "Reasearch & Development"}],
+    description: "Money to fund the bitcoin project",
+    bannerImage: "mockbannerimages.com",
+    goalAmount: 50,
+    updates: "Things are going well",
+    shareLink: "bitcoin-fund",
+    owner: data[0]
+
+  },  {
+    title: "Dogecoin fund",
+    category: [{type: "Reasearch & Development"}],
+    description: "Money to fund the Dogecoin project",
+    bannerImage: "mockbannerimages.com",
+    goalAmount: 50,
+    updates: "Doge is best",
+    shareLink: "doge-fund",
+    owner: data[0]
+
+  }
+]
+
+export const categoryData: Category[] = [
+  {type: "Research & Development"}
+]
